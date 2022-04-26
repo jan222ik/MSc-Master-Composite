75db0a4e3a374f35ff5e0691f96fcf2a397ef872
jan222ik <jan.222.ik@gmail.com>
2022-04-26 15:14:46 +0200

    [MAS-XXX] Fixed dndHandler issue for dropping on diagrams

--- combined/src/main/kotlin/com/github/jan222ik/ui/components/dnd/dndDropTarget.kt
+++ combined/src/main/kotlin/com/github/jan222ik/ui/components/dnd/dndDropTarget.kt
@@ -12,15 +12,17 @@ fun Modifier.dndDropTarget(
     handler: DnDHandler,
     dropActions: DnDAction
 ): Modifier {
-    var state by remember { mutableStateOf<LayoutCoordinates?>(null) }
-
-    return this.then(Modifier.onGloballyPositioned {
+    var state by remember(dropActions.name) { mutableStateOf<LayoutCoordinates?>(null) }
+    val mod = remember(dropActions.name) {
+        handler.dropTargets.value = handler.dropTargets.value.toMutableList().filterNot { it.second.name == dropActions.name }
+        Modifier.onGloballyPositioned {
         if (state != it) {
             val list = handler.dropTargets.value.toMutableList()
             state?.to(dropActions)?.let(list::remove)
-            list += it to dropActions
+            list.add(0, it to dropActions)
             handler.dropTargets.value = list
             state = it
         }
-    })
+    } }
+    return this.then(mod)
 }
